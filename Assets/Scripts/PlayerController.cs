8ecbad48716f0c39e4307dc71deaaee73a09afcd
iCOOKI3 <bryantan2001@gmail.com>
2021-02-10 20:25:03 +0800

    Update1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    public Animator playerAnim;
+    public Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+
+        playerRb = GetComponent<Rigidbody>();
+
+        playerAnim.SetBool("isIdle",true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Front
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+
+            playerAnim.SetBool("isIdle", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.SetBool("isIdle", true);
+        }
+        //Back
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+
+            playerAnim.SetBool("isIdle", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.SetBool("isIdle", true);
+        }
+        //Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+        //Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+    }
+}
